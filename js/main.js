2added39f0e7f3306aac84fb4a8ba13b579e051e
diegomompo7 <diego.mompo.redoli@gmail.com>
2023-11-18 23:15:40 +0100

    Offers

--- js/main.js
+++ js/main.js
@@ -45,3 +45,7 @@ const swiper = new Swiper('.mySwiper', {
 
 
 //
+
+const sendAlert = () => {
+    alert("¡Thank you for your request! We have received it correctly. Someone from our Team will get back to you very soon.The Miranda Hotel")
+}
